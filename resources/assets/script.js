b99de62181be9b9d902874d67c067262cf5cffba
RPAPE3 <106628145+RPAPE3@users.noreply.github.com>
2022-06-18 14:54:37 -0700

    added a retrieve question function that still needs some testing

--- resources/assets/script.js
+++ resources/assets/script.js
@@ -5,6 +5,11 @@
 var start_menu = document.querySelector(".card")
 var start_btn = document.querySelector(".start");
 var q_box = document.querySelector(".q_box");
+var question = document.querySelector(".question");
+var choice_A = document.querySelector(".choice_A");
+var choice_B = document.querySelector(".choice_B");
+var choice_C = document.querySelector(".choice_C");
+var choice_D = document.querySelector(".choice_D");
 
 
 var question_key = [
@@ -15,13 +20,43 @@ var question_key = [
         optionC: "no",
         optionD: "yes",
         correct: "D"
+    }, {
+        question: "question 2 test",
+        optionA: "no",
+        optionB: "no",
+        optionC: "yes",
+        optionD: "no",
+        correct: "C"
+    }, {
+        question: "question 3 test",
+        optionA: "no",
+        optionB: "yes",
+        optionC: "no",
+        optionD: "no",
+        correct: "B"
+    }, {
+        question: "question 4 test",
+        optionA: "yes",
+        optionB: "no",
+        optionC: "no",
+        optionD: "no",
+        correct: "A"
     }
-]
+];
 
 start_btn.addEventListener("click", beginQuiz); 
 
 function beginQuiz () {
     start_menu.style.display = "none";
     q_box.style.display = "block";
+    retrieve_question();
 };
 
+function retrieve_question () {
+    var question_screen = question_key[0];
+    question.innerHTML = question_screen.question;
+    choice_A.innerHTML = question_screen.optionA;
+    choice_B.innerHTML = question_screen.optionB;
+    choice_C.innerHTML = question_screen.optionC;
+    choice_D.innerHTML = question_screen.optionD;
+}
