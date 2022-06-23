f957574858077599affff3ba1b0d1b5acb20082f
RPAPE3 <106628145+RPAPE3@users.noreply.github.com>
2022-06-22 17:51:02 -0700

    updated score screen with user int input

--- resources/assets/script.js
+++ resources/assets/script.js
@@ -1,49 +1,50 @@
-// var startBtn = document.querySelector(".start");
-// startBtn.innerHTML = "Start";
-// // document.body.childe.appendChild(startBtn);
-
 var start_menu = document.querySelector(".card")
 var start_btn = document.querySelector(".start");
 var q_box = document.querySelector(".q_box");
 var timer = document.querySelector(".timer")
+var high_score = document.querySelector(".high_score")
 var question = document.querySelector(".question");
 var choice_A = document.querySelector(".choice_A");
 var choice_B = document.querySelector(".choice_B");
 var choice_C = document.querySelector(".choice_C");
 var choice_D = document.querySelector(".choice_D");
 var s_box = document.querySelector(".s_box");
-var stop_timer = document.querySelector(".stop_timer")
+var stop_timer = document.querySelector(".stop_timer");
+var final_score = document.querySelector(".final_score");
+var enter_score = document.querySelector(".enter_score");
+var user_input = document.querySelector(".user_input");
+var score_display = document.querySelector(".score_display");
 
 
 var question_key = [
     {
-        question: "question 1",
-        optionA: "no",
-        optionB: "no",
-        optionC: "no",
-        optionD: "D",
-        correct: "D"
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        optionA: "var",
+        optionB: "const",
+        optionC: "let",
+        optionD: "All of the above",
+        correct: "All of the above"
     }, {
-        question: "question 2",
-        optionA: "no",
-        optionB: "no",
-        optionC: "C",
-        optionD: "no",
-        correct: "C"
+        question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        optionA: "Boolean",
+        optionB: "Undefined",
+        optionC: "Object",
+        optionD: "Integer",
+        correct: "Object"
     }, {
-        question: "question 3",
-        optionA: "no",
-        optionB: "B",
-        optionC: "no",
-        optionD: "no",
-        correct: "B"
+        question: "How to stop an interval timer in Javascript",
+        optionA: "clearTimer",
+        optionB: "clearInterval",
+        optionC: "clearClock",
+        optionD: "clearWatch",
+        correct: "clearInterval"
     }, {
-        question: "question 4",
-        optionA: "A",
-        optionB: "no",
-        optionC: "no",
-        optionD: "no",
-        correct: "A"
+        question: "How do we write a comment in javascript?",
+        optionA: "//",
+        optionB: "/* */",
+        optionC: "#",
+        optionD: "$ $",
+        correct: "//"
     }, {
         end: "end"
     }
@@ -66,116 +67,16 @@ function timeClock () {
     setInterval(function() {
         secondsLeft--; 
         timer.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            scoreScreen();
+        }
     }, 1000);
 };
 
 function time_minus_10 () {
     secondsLeft = secondsLeft - 10;
 };
-
-
-
-
-// function retrieve_question () {
-//     var question_screen = question_key[0];
-//     question.innerHTML = question_screen.question;
-//     choice_A.innerHTML = question_screen.optionA;
-//     choice_B.innerHTML = question_screen.optionB;
-//     choice_C.innerHTML = question_screen.optionC;
-//     choice_D.innerHTML = question_screen.optionD;
-
-//     choice_D.addEventListener("click", retrieve_question_2);
-
-//     choice_A.addEventListener("click", time_minus_10);
-//     choice_A.addEventListener("click", retrieve_question_2);
-
-//     choice_B.addEventListener("click", time_minus_10);
-//     choice_B.addEventListener("click", retrieve_question_2);
-
-//     choice_C.addEventListener("click", time_minus_10);
-//     choice_C.addEventListener("click", retrieve_question_2);
-
-//     // questionIndex++
-// };
-
-// function retrieve_question_2 () {
-
-//     var question_screen = question_key[1];
-//     question.innerHTML = question_screen.question;
-//     choice_A.innerHTML = question_screen.optionA;
-//     choice_B.innerHTML = question_screen.optionB;
-//     choice_C.innerHTML = question_screen.optionC;
-//     choice_D.innerHTML = question_screen.optionD;
-
-//     choice_C.removeEventListener("click", time_minus_10);
-//     choice_C.addEventListener("click", retrieve_question_3);
-
-//     choice_A.addEventListener("click", time_minus_10);
-//     choice_A.addEventListener("click", retrieve_question_3);
-//     choice_B.addEventListener("click", time_minus_10);
-//     choice_B.addEventListener("click", retrieve_question_3);
-//     choice_D.addEventListener("click", time_minus_10);
-//     choice_D.addEventListener("click", retrieve_question_3);
-
-//     // choice_A.removeEventListener("click", time_minus_10);
-//     // choice_B.removeEventListener("click", time_minus_10);
-//     // choice_C.removeEventListener("click", time_minus_10);
-//     // choice_D.removeEventListener("click", time_minus_10);
-        
-// };
-
-
-// function retrieve_question_3 () {
-
-//     var question_screen = question_key[2];
-//     question.innerHTML = question_screen.question;
-//     choice_A.innerHTML = question_screen.optionA;
-//     choice_B.innerHTML = question_screen.optionB;
-//     choice_C.innerHTML = question_screen.optionC;
-//     choice_D.innerHTML = question_screen.optionD;
-
-//     choice_B.addEventListener("click", retrieve_question_4);
-//     choice_B.removeEventListener("click", time_minus_10);
-
-//     choice_A.addEventListener("click", time_minus_10);
-//     choice_A.addEventListener("click", retrieve_question_4);
-//     choice_C.addEventListener("click", time_minus_10);
-//     choice_C.addEventListener("click", retrieve_question_4);
-//     choice_D.addEventListener("click", time_minus_10);
-//     choice_D.addEventListener("click", retrieve_question_4);
-
-//     // choice_A.removeEventListener("click", time_minus_10);
-//     // choice_B.removeEventListener("click", time_minus_10);
-//     // choice_C.removeEventListener("click", time_minus_10);
-//     // choice_D.removeEventListener("click", time_minus_10);
-
-// };
-
-// function retrieve_question_4 () {
-
-//     var question_screen = question_key[3];
-//         question.innerHTML = question_screen.question;
-//         choice_A.innerHTML = question_screen.optionA;
-//         choice_B.innerHTML = question_screen.optionB;
-//         choice_C.innerHTML = question_screen.optionC;
-//         choice_D.innerHTML = question_screen.optionD;
-
-//     choice_A.addEventListener("click", scoreScreen);
-//     choice_A.removeEventListener("click", time_minus_10);
-
-//     choice_B.addEventListener("click", time_minus_10);
-//     choice_B.addEventListener("click", scoreScreen);
-//     choice_C.addEventListener("click", time_minus_10);
-//     choice_C.addEventListener("click", scoreScreen);
-//     choice_D.addEventListener("click", time_minus_10);
-//     choice_D.addEventListener("click", scoreScreen);
-// };
-
-function scoreScreen () {
-    q_box.style.display = "none";
-    s_box.style.display = "block";
-    stop_timer.textContent = secondsLeft;
-}
 
 function retrieve_question () {
     var question_screen = question_key[questionIndex];
@@ -198,17 +99,12 @@ function next_question () {
     choice_C.innerHTML = question_screen.optionC;
     choice_D.innerHTML = question_screen.optionD;
 }
-// var question_screen = question_key[questionIndex];
-// question.innerHTML = question_screen.question;
-// choice_A.innerHTML = question_screen.optionA;
-// choice_B.innerHTML = question_screen.optionB;
-// choice_C.innerHTML = question_screen.optionC;
-// choice_D.innerHTML = question_screen.optionD;
 
+// added event listener to the document. 
 document.addEventListener("click", function (event) {
-    
+// specified the target of the click to class ".choices"
     var target = event.target; 
-
+// if the textContent of the click from ".choices" equals the value of .correct from the question_key array, it will bring you to next set up questions by calling the next_question function. If not then it will subtract 10 seconds from the timer by calling the time_minus_10 and also bring you to the next question.
     if (target.matches(".choice")){
         if (question_key[questionIndex].correct === event.target.textContent) {
             next_question();
@@ -216,11 +112,32 @@ document.addEventListener("click", function (event) {
             next_question();
             time_minus_10();
         }
-
+// once we hit index 4 of the question_key array it will bring us to the score screen by calling the scoreScreen function. 
         if (questionIndex === 4) {
             scoreScreen();
         }
     }
 })
 
+function scoreScreen () {
+    q_box.style.display = "none";
+    s_box.style.display = "block";
 
+    stop_timer.textContent = secondsLeft;
+    final_score.textContent = ('Final Score:' + secondsLeft);
+}
+
+document.addEventListener("click", function(event) {
+    var target = event.target;
+    
+    if (target.matches(".submit")) {
+        var inputValue = user_input.value; 
+        var listEl = document.createElement("li");
+        listEl.textContent = (inputValue + " - " + stop_timer.textContent)
+        score_display.appendChild(listEl);
+
+        user_input.value = '';
+    }
+})
+
+high_score.addEventListener("click", scoreScreen);
